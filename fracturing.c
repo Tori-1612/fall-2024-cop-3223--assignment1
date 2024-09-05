c273f5ed93bfbcd9f0b0bbfd3dfd7cdab88c8423
Tori-1612 <tori.casto22@gmail.com>
2024-09-05 15:18:36 +0000

    finally done

--- fracturing.c
+++ fracturing.c
@@ -1,96 +1,108 @@
 // Tori Casto
 // UCFID: 3978588
+// September 5th, 2024
+// Assignment 1- Fracturing
+// Professor Parra
 
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
 
-double distancestatement(){
+// a helper function that will be used for user input in other functions
+double distancestatement()
+{
+    double x1,x2,y1,y2,totalDistance; // variables that will be used for points on a circle
 
-    double x1,x2,y1,y2,totalDistance;
+    // Ask the user for the 'x' values of each point
 
     printf("Enter two numbers for the 'x' values (x1,x2)\n");
     scanf("%lf%lf", &x1, &x2);
 
+    // Ask the user for the 'y' values of each point
+
     printf("Enter two numbers for the 'y' values (y1,y2)\n");
     scanf("%lf%lf", &y1, &y2);
+
+    // Outputs both 'x' and 'y' values entered by the user
 
     printf("Point #1 entered: x1 = %.2lf; y1 = %.2lf \n", x1, y1);
     printf("Point #2 entered: x2 = %.2lf; y2 = %.2lf \n", x2, y2);
 
-    totalDistance= sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-
+    totalDistance= sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); // will calculate the distance between the given points
     return totalDistance;
 }
 
-double calculateDistance(){
-    
-    double distance=0;
+double calculateDistance()
+{    
+    double distance=0; // variable used to capture distance
 
-    distance=distancestatement();
+    distance=distancestatement(); // calls the helper function to gather the 'x' and 'y' points being used
+
+    // outputs the distance between the two points given from the helper function using the equation totalDistance in distancestatement
 
     printf("The distance between the two points is %.3lf \n", distance);
-
     return distance;
 }
 
-double calculatePerimeter(){
+double calculatePerimeter()
+{
+    double totalPerimeter=0; // used to capture the perimeter
+    double radius=0; // used to capture the radius
 
-    double totalPerimeter=0;
+    radius=distancestatement()/2; // calculating the radius using the helper function distancestatement
 
-    double radius=0;
+    totalPerimeter=2*PI*radius; // calculating the perimeter of a circle 
 
-    radius=distancestatement() / 2;
-
-    totalPerimeter=2*PI*radius;
+    // will output the perimeter 
     
     printf("The perimeter of the city encompassed by your request is %.3lf \n", totalPerimeter);
 
-    double difficultyLevel=3.5;
-
+    double difficultyLevel=3.5; // the difficulty level at which I found creating this function to be
     return difficultyLevel;
 }
 
 double calculateArea(){
     
-    double totalArea=0;
+    double totalArea=0; // used to capture the area of the circle
+    double radius=0; // used to capture the radius
 
-    double radius=0;
+    radius=distancestatement()/2; // calculating the radius using the helper function distancestatement
 
-    radius=distancestatement() / 2;
+    totalArea=PI*radius*radius;  // calculating the area of a circle 
 
-    totalArea=PI*radius*radius;
+    // will output the area
     
     printf("The area of the city encompassed by your request is %.3lf \n", totalArea);
 
-    double difficultyLevel=2.0;
-    
+    double difficultyLevel=2.0;  // the difficulty level at which I found creating this function to be
     return difficultyLevel;
 }
 
 double calculateWidth(){
 
-    double width=0;
+    double width=0; // used to capture the width of the circle
 
-    width=distancestatement();
+    width=distancestatement(); // calculating the width using the helper function distancestatement
+
+    // will output the width
 
     printf("The width of the city encompassed by your request is %.3lf \n",width);
 
-    double difficultyLevel=1.5; 
-
+    double difficultyLevel=1.5; // the difficulty level at which I found creating this function to be
     return difficultyLevel;
 }
 
 double calculateHeight(){
 
-    double height=0;
+    double height=0; // used to capture the height of the circle
 
-    height=distancestatement();
+    height=distancestatement(); // calculating the height using the helper function distancestatement
+
+    // will output the width
 
     printf("The height of the city encompassed by your request is %.3lf \n",height);
 
-    double difficultyLevel=1.0;
-
+    double difficultyLevel=1.0;  // the difficulty level at which I found creating this function to be
     return difficultyLevel;
 }
 
@@ -100,6 +112,5 @@ int main(int argc, char**argv){
     calculateArea();
     calculateWidth();
     calculateHeight();
-
     return 0;
 }
